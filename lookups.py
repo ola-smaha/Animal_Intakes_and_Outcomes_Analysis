7c56fc3b63cf78ac36dfcf47da26a33cbc527535
ola-smaha <olasmaha@gmail.com>
2023-10-10 21:40:11 +0300

    Cleaning of two datasets

--- lookups.py
+++ lookups.py
@@ -6,8 +6,8 @@ class Errors(Enum):
 
 class DataSources(Enum):
     SHELTER_SONOMA = "https://data.sonomacounty.ca.gov/resource/924a-vesw.json?$select=id,type,breed,color,sex,date_of_birth,intake_date,outcome_date,intake_type,outcome_type"
-    SHELTER_AUSTIN_INTAKES = "https://data.austintexas.gov/resource/wter-evkm.json?$select=animal_id,animal_type,breed,color,sex_upon_intake,age_upon_intake,datetime,intake_type"
-    SHELTER_AUSTIN_OUTCOMES = "https://data.austintexas.gov/resource/9t4d-g238.json?$select=animal_id,sex_upon_outcome,age_upon_outcome,date_of_birth,datetime,outcome_type"
+    SHELTER_AUSTIN_INTAKES = "https://data.austintexas.gov/resource/wter-evkm.json?$select=animal_id,animal_type,breed,color,datetime,intake_type"
+    SHELTER_AUSTIN_OUTCOMES = "https://data.austintexas.gov/resource/9t4d-g238.json?$select=animal_id,sex_upon_outcome,date_of_birth,datetime,outcome_type"
     SHELTER_NORFOLK = "https://data.norfolk.gov/resource/vfm4-5wv6.json?$select=animal_id,animal_type,primary_breed,primary_color,sex,years_old,months_old,intake_date,outcome_date,intake_type,outcome_type"
     SHELTER_BLOOMINGTON = "https://data.bloomington.in.gov/resource/e245-r9ub.json?$select=id,speciesname,breedname,basecolour,sexname,animalage,intakedate,movementdate,intakereason,movementtype"
     SHELTER_DALLAS_2017_2018 = "https://www.dallasopendata.com/resource/wb7n-sdxi.json?$select=animal_id,animal_type,animal_breed,intake_date,intake_time,outcome_date,outcome_time,intake_type,outcome_type"

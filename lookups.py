26cda328d6196927f8edad13a57bcddb76167e79
ola-smaha <olasmaha@gmail.com>
2023-10-27 02:06:13 +0300

    added a type of error to transformation errors Enum class

--- lookups.py
+++ lookups.py
@@ -26,6 +26,7 @@ class TransformationErrors(Enum):
     CLEAN_SONOMA_DF_ERROR = "Error doing pandas transformations on Sonoma shelter dataset"
     TRANSFORM_UNEMPLOYMENT_DATA = "Error transforming an 'unemployement' dataframe"
     CLEAN_ALL_DATA = "Error in clean_all_data"
+    CLEAN_DEMOGRAPHIC_DATA = "clean_demographic_dfs"
 
 class DataSources(Enum):
     SHELTER_SONOMA = "https://data.sonomacounty.ca.gov/resource/924a-vesw.json?$select=id,type,breed,color,sex,date_of_birth,intake_date,outcome_date,intake_type,outcome_type"

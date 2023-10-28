38f08c3cc03e3ef253a9dfffebcf3908c48d909d
ola-smaha <olasmaha@gmail.com>
2023-10-29 00:58:02 +0200

    altered transformation file to make code cleaner

--- transformation_handler.py
+++ transformation_handler.py
@@ -1,100 +1,12 @@
 # Reading sources + basic transformation using pandas
-import requests
 import pandas as pd
 import numpy as np
-from io import StringIO
-from concurrent.futures import ThreadPoolExecutor
-from lookups import DataSources, TransformationErrors, IntakesOutcomesTablesNames, DateCondition
+from lookups import TransformationErrors, IntakesOutcomesTablesNames, Open_AI
 from logging_handler import log_error_msg
 import warnings
-from bs4 import BeautifulSoup
 import json
-
-def get_data_columns(sources):
-    shelter_sources = [source for source in sources if source.name.startswith('SHELTER')]
-    data = {}
-    for source in shelter_sources:
-        response = requests.get(source.value,params={'$limit': 1})
-        result = (source.name.lower(), pd.DataFrame(response.json()))
-        columns = result[1].columns
-        data[result[0]] = columns.tolist()
-    return data
-
-def fetch_data(source, limit, etl_date):
-    result = None
-    condition = None
-    try:
-        if source.name.startswith("SHELTER"):
-            if etl_date != None:
-                if source.name.startswith('SHELTER_AUSTIN'):
-                    condition = DateCondition.AUSTIN.value.replace('0',f"'{etl_date}'")
-                elif source == DataSources.SHELTER_BLOOMINGTON:
-                    condition = DateCondition.BLOOMINGTON_INTAKES.value.replace('0',f"'{etl_date}'")
-                elif source in [DataSources.SHELTER_SONOMA,DataSources.SHELTER_NORFOLK] or source.name.startswith("SHELTER_DALLAS"):
-                    condition = DateCondition.OTHER.value.replace('0',f"'{etl_date}'")
-            response = requests.get(source.value, params={'$limit': limit, '$where':condition})
-            result = (source.name.lower(), pd.DataFrame(response.json()))
-            if result[1].empty:
-                column_names_dict = get_data_columns(DataSources)
-                result = (result[0], pd.DataFrame(columns=column_names_dict[result[0]]))
-        elif source.name.startswith("POPULATION"):
-            response = requests.get(source.value)
-            df = pd.read_csv(StringIO(response.text))
-            df.columns = ['date', source.name.split('_')[1].title()]
-            result = (source.name.lower(), df)
-        elif source.name.split('_')[1] == "POPULATION":
-            response = requests.get(source.value)
-            result = ('_'.join(source.name.split('_')[::-1]).lower(), BeautifulSoup(response.text,'lxml'))
-        elif source.name.startswith('PER_CAPITA') or source.name.startswith('UNEMPLOYMENT'):
-            response = requests.get(source.value)
-            df = pd.read_csv(StringIO(response.text))
-            df.columns = ['date', source.name.split('_')[2].title()]
-            result = (source.name.lower(), df)         
-    except Exception as e:
-        log_error_msg(TransformationErrors.FETCHING_DATA_FROM_SOURCE.value, str(e))
-    finally:
-        return result
-
-
-def web_scrape_data(soup):
-    table = soup.find_all('table')[0]
-    columns = table.find_all('th')
-    columns = [column.text.strip() for column in columns]
-    df = pd.DataFrame(columns=columns)
-    rows = table.find_all('tr')
-    for row in rows[1:]:
-        row_data = row.find_all('td')
-        single_row_data = [data.text.strip() for data in row_data]
-        length = len(df)
-        df.loc[length] = single_row_data
-    df.drop(df.columns[2], axis = 1, inplace=True)
-    df.iloc[:,0] = [i.replace('*','').strip() for i in df.iloc[:,0]]
-    df.iloc[:,1] = [i.replace(',','').strip() for i in df.iloc[:,1]]
-    df = df.astype(np.int64)
-    return df
-
-
-def readData(etl_date = None, limit = None): 
-    data_dict = dict()
-    try:
-        sources = list(DataSources)
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            results = list(executor.map(lambda source: fetch_data(source, limit, etl_date), sources))
-            for result in results:
-                if  result[1] is not None:
-                    if isinstance(result[1], pd.DataFrame):
-                        data_dict[result[0]] = result[1]
-                    elif isinstance(result[1], BeautifulSoup):
-                        df = web_scrape_data(result[1])
-                        df.columns = ['year', result[0].split('_')[1].title()]
-                        data_dict[result[0]] = df
-                else:
-                    raise Exception(f"{TransformationErrors.FETCHING_DATA_FROM_SOURCE.value}: a result df is None.")
-    except Exception as e:
-        log_error_msg(TransformationErrors.READ_DATA_FN_ERROR.value,str(e))
-    finally:
-        return data_dict
-
+from data_extraction_handler import *
+from openai_handler import get_gpt_animal_type_string
 
 def clean_sonoma_dataset(dfs):
     sonoma = None
@@ -281,6 +193,7 @@ def transform_unemployment_data(df):
         log_error_msg(TransformationErrors.TRANSFORM_UNEMPLOYMENT_DATA.value, str(e))
     finally:
         return annual_unemployment_df
+    
 
 def edit_animal_type(df,ai_list,animal_type):
     lst = []
@@ -294,19 +207,21 @@ def edit_animal_type(df,ai_list,animal_type):
     except Exception as e:
         log_error_msg(TransformationErrors.EDIT_ANIMAL_TYPE.value, str(e))
 
-def edit_all_types(dfs):
+
+def edit_all_animal_types(dfs):
     try:
-        with open("openai_animal_types.json", "r") as json_file:
-            data = json.load(json_file)
-        birds_list = data['bird'].split(', ')
+        bird_types = get_gpt_animal_type_string(content_name = Open_AI.BIRD_TYPE, content= Open_AI.GPT_BIRD_CONTENT)
+        livestock_type = get_gpt_animal_type_string(content_name = Open_AI.LIVESTOCK_TYPE, content= Open_AI.GPT_LIVESTOCK_CONTENT)
+        birds_list = bird_types.split(', ')
         birds_list = [i.title() for i in birds_list]
-        livestock_list = data['livestock'].split(', ')
+        livestock_list = livestock_type.split(', ')
         livestock_list = [i.title() for i in livestock_list]
         for df in dfs:
             edit_animal_type(df,birds_list,'Bird')
             edit_animal_type(df,livestock_list,'Livestock')
     except Exception as e:
         log_error_msg(TransformationErrors.EDIT_ANIMAL_TYPE.value, str(e))
+
 
 def clean_demographic_dfs(dfs):
     try:
@@ -330,6 +245,7 @@ def clean_demographic_dfs(dfs):
     finally:
         return clean_data_dict
 
+
 def clean_all_data(dfs):
     clean_data_dict = {}
     try:
@@ -348,11 +264,11 @@ def clean_all_data(dfs):
             df[date_columns] = df[date_columns].fillna('1700-01-01').astype('datetime64[ns]')
             df['breed'] = df['breed'].replace({
                 'Short Hair|Shorthair': 'Sh','Medium Hair|Mediumhair':'Mh','Long Hair|Longhair':'Lh',
-                'Amer |Am ': 'American', 'Aust ':'Australian', 'Belg ':'Belgian', 'Alask ':'Alaskan',
-                'Anatol ':'Anatolian', 'Eng ': 'English', 'Retriever':'Retr', 'Min':'Miniature'},regex=True)
+                'Amer |Am ': 'American ', 'Aust ':'Australian ', 'Belg ':'Belgian ', 'Alask ':'Alaskan ',
+                'Anatol ':'Anatolian ', 'Eng ': 'English ', 'Retriever':'Retr', 'Min':'Miniature', 'Pit Bull': 'Pitbull'},regex=True)
             df.dropna(subset=['intake_type'], inplace=True)
             df.drop(df[(df['outcome_date'] < df['intake_date']) & (df['outcome_date'] != pd.Timestamp('1700-01-01'))].index, inplace=True)
-        edit_all_types(intakes_dfs)
+        edit_all_animal_types(intakes_dfs)
         for df in intakes_dfs:
             df.loc[df['breed'] == 'Guinea Pig', 'type'] = 'Other'
             

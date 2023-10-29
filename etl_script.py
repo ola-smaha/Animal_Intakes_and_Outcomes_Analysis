f1cfeb16c543e169c4279509ccc8afd00bb4f39b
ola-smaha <olasmaha@gmail.com>
2023-10-29 03:38:31 +0200

    edited main file

--- etl_script.py
+++ etl_script.py
@@ -1,7 +1,9 @@
+from logging_handler import setup_logging
 from prehook import execute_prehook
 from hook import execute_hook
 from posthook import execute_posthook
 
-execute_prehook()
-execute_hook()
-execute_posthook()
+logger = setup_logging()
+execute_prehook(logger)
+execute_hook(logger)
+execute_posthook(logger)

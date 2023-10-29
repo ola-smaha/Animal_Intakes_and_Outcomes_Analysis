237cbefd8d50910f7a56ec44102ab2b599734bf1
ola-smaha <olasmaha@gmail.com>
2023-10-29 02:23:23 +0200

    added logging for execute posthook function

--- posthook.py
+++ posthook.py
@@ -16,18 +16,36 @@ def truncate_staging_table(db_session, source_name=DataWareHouseSchema.SCHEMA_NA
     except Exception as e:
         log_error_msg(Errors.POSTHOOK_TRUNCATE_ERROR.value, str(e))
 
-def execute_posthook():
+# def execute_posthook():
+#     try:
+#         step = 1
+#         db_session = create_connection()
+#         step = 2
+#         truncate_staging_table(db_session, source_name = DataWareHouseSchema.SCHEMA_NAME)
+#         step = 3
+#         close_connection(db_session)
+#     except Exception as e:
+#         log_error_msg(f"{Errors.EXECUTE_POSTHOOK_ERROR.value}: error on step {step}", str(e))
+
+def execute_posthook(logger):
+    step = None
+    logger.info("Executing posthook:")
     try:
         step = 1
+        logger.info("Step 1: Creating a database connection")
         db_session = create_connection()
+
         step = 2
-        truncate_staging_table(db_session, source_name = DataWareHouseSchema.SCHEMA_NAME)
+        logger.info("Step 2: Truncating staging table")
+        truncate_staging_table(db_session, source_name=DataWareHouseSchema.SCHEMA_NAME)
+
         step = 3
+        logger.info("Step 3: Closing the database connection\n")
         close_connection(db_session)
+
     except Exception as e:
-        log_error_msg(f"{Errors.EXECUTE_POSTHOOK_ERROR.value}: error on step {step}", str(e))
-
-
+        error_prefix = f'{Errors.EXECUTE_POSTHOOK_ERROR.value} - Error on step {step}'
+        logger.error(f'{error_prefix} - {str(e)}')
 
 
 

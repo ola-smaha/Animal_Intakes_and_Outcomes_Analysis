00a967ce7e5ff8181fed50f6d72ac44bf0b3cd02
ola-smaha <olasmaha@gmail.com>
2023-10-30 14:07:08 +0200

    handled errors

--- misc_handler.py
+++ misc_handler.py
@@ -1,7 +1,13 @@
 import os
-from lookups import SQLCommandsPath
+from lookups import SQLCommandsPath, Errors
+from logging_handler import log_error_msg
 
 def get_sql_files_list(sql_commands_path = SQLCommandsPath.SQL_FOLDER):
-    sql_files = [file for file in os.listdir(sql_commands_path.value) if file.endswith('.sql')]
-    sorted_sql_files = sorted(sql_files)
-    return sorted_sql_files
+    sql_files = []
+    try:
+        sql_files = [file for file in os.listdir(sql_commands_path.value) if file.endswith('.sql')]
+        sorted_sql_files = sorted(sql_files)
+    except Exception as e:
+        log_error_msg(Errors.GET_SQL_FILES_LIST.value, str(e))
+    finally:
+        return sorted_sql_files
